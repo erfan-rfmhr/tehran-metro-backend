1a04709af04e71e5b9295e224426022e3fdd780e
Erfan Arefmehr <erfan.arefmehr@gmail.com>
2024-10-19 23:56:15 +0330

    feat: create lifespan util

---
+++ config/utils.py
@@ -0,0 +1,20 @@
+from contextlib import asynccontextmanager
+
+from config.db import Database
+
+
+async def on_startup():
+    # on startup event for lifespan
+    await Database.initialize()
+
+
+async def on_shutdown():
+    # on shutdown event for lifespan
+    await Database().driver.close()
+
+
+@asynccontextmanager
+async def lifespan():
+    await on_startup()
+    yield
+    await on_shutdown()
