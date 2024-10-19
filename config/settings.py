6ea2cf19163b355b870cd11b14b1f4ff8115829f
Erfan Arefmehr <erfan.arefmehr@gmail.com>
2024-10-19 21:28:23 +0330

    feat: add neo4j settings

---
+++ config/settings.py
@@ -0,0 +1,7 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    NEO4J_URI: str = "bolt://localhost:7687"
+    NEO4J_USERNAME: str = "neo4j"
+    NEO4J_PASSWORD: str = "12345678"
