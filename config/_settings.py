a6c5430ba3fd13d5969f8b361d0edcdc1febc83b
Erfan Arefmehr <erfan.arefmehr@gmail.com>
2024-10-19 22:21:23 +0330

    feat: implement get_settings method

--- config/_settings.py
+++ config/_settings.py
@@ -5,3 +5,7 @@ class Settings(BaseSettings):
     NEO4J_URI: str = "bolt://localhost:7687"
     NEO4J_USERNAME: str = "neo4j"
     NEO4J_PASSWORD: str = "12345678"
+
+
+def get_settings():
+    return Settings()
